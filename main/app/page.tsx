571bb380ce547fe7b7e08fe4ae251701eecb90f2
yuvic191212 <510039341@dclive.ca>
2025-05-31 01:47:48 +0000

    "good comit"

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
 export default function Page() {
     return (
         < >
         <h2> welcome to yengine</h2>
         <h3> yengine is a wip game engine</h3>
         <p> it is writen in next.js and allows coding in js jsx ts python(brython) java(cherpj) and react using webgl for graphics handling</p>
+        <Link href="/test"> <h2> test</h2></Link>
         </>
     )
 }
---
+++ main/app/test/page.tsx
@@ -0,0 +1,10 @@
+//get the time
+var time = new Date().toLocaleTimeString
+export default function Page(){
+    return(
+        < >
+       <h1> (time) </h1>
+        </>
+    )
+
+}

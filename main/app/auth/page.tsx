7c86c162a3fb6d1f34352955dde16da3bb245219
yuvic191212 <510039341@dclive.ca>
2025-06-05 17:22:05 +0000

    h

--- main/app/auth/page.tsx
+++ main/app/auth/page.tsx
@@ -1,12 +1,11 @@
-import { Redirect } from "next"
+import { redirect } from "next/navigation"
 import { headers } from "next/headers"
-import { Suspense } from "react"
 
 export default function AuthPage({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
     const redirectsite = searchParams.redirect || "/"
     const cookie = headers().get("cookie")
     if (cookie && cookie.includes("auth_token")) {
-        return <Redirect to={redirectsite} />
+        redirect(redirectsite)
     }
     return (
         <>
@@ -14,32 +13,23 @@ export default function AuthPage({ searchParams }: { searchParams: { [key: strin
             <h3>after you login you will be redirected to:</h3>
             <br />
             <h1>{redirectsite}</h1>
-            <Suspense fallback={<div className="box">Loading...</div>}>
-                <Login />
-            </Suspense>
-            <Suspense fallback={<div className="box">Loading...</div>}>
-                <Register />
-            </Suspense>
+            <Login />
+            <Register />
         </>
     )
 }
 
-// Simulate async server component with delay
-async function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
-export async function Login() {
+function Login() {
     return <div className="box">
         <h1>Login</h1>
     </div>
 }
 
-export async function Register() {
+function Register() {
     return <div className="box">
-          <h1>Register</h1>
-          <fourm>
-            <input type=text name="username"/>
-          </fourm>
-         </div>
+        <h1>Register</h1>
+        <form>
+            <input type="text" name="username" />
+        </form>
+    </div>
 }
